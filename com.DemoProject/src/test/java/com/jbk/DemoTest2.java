b7bb6c4205b41808ffbc57f4e1381990e61b9208
HP <HP@DESKTOP-122O0EN>
2023-03-27 07:33:33 -0700

    DemoProject1

---
+++ com.DemoProject/src/test/java/com/jbk/DemoTest2.java
@@ -0,0 +1,14 @@
+package com.jbk;
+
+import org.testng.annotations.Test;
+
+public class DemoTest2 {
+
+	@Test
+	public void test01() 
+	{
+		System.out.println("Hello");
+	}
+	
+	
+}
---
+++ com.DemoProject/src/test/java/com/tests/DemoTest.java
@@ -0,0 +1,14 @@
+package com.tests;
+
+import org.testng.annotations.Test;
+
+public class DemoTest {
+	
+	@Test
+	public void test01() 
+	{
+		System.out.println("Hi");
+	}
+	
+
+}
